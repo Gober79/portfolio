374c9ab2375db2b547e50dfb8733f2b7967e4689
Gober79 <79615785+Gober79@users.noreply.github.com>
2021-02-24 21:00:55 -0500

    Add files via upload

---
+++ ButtonTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class ButtonTest: MonoBehaviour
+{
+    public Text Davidisa;
+
+    public void whatisdavid()
+    {
+        Davidisa.text = "A really nice guy";
+    }
+    public void buttonexit()
+    {
+        Application.Quit();
+    }
+}
---
+++ InputBoxCode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputBoxCode : MonoBehaviour
+{
+    public string Response;
+    public InputField Guess;
+    public Text Done;
+
+    public void Answer()
+    {
+        Response = Guess.text;
+
+        if (Response == "4")
+        {
+            Done.text = "Correct!!!";
+        }
+        else
+        {
+            Done.text = "Incorrect!!!";
+        }
+    }
+}
